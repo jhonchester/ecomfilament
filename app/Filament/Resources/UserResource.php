bde9d383c421ff306f059d22b160a6243c75f848
jhonchester <jhonchesterguijoba186@gmail.com>
2024-11-15 19:47:10 +0800

    updated categories and products

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\UserResource\Pages;
 use App\Filament\Resources\UserResource\RelationManagers;
 use App\Models\User;
+use App\Filament\Resources\GroupResource;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Pages\CreateRecord;
