4bef26274742eb70b78b2087622c78e555d2191e
jhonchester <jhonchesterguijoba186@gmail.com>
2024-11-16 18:48:30 +0800

    add functionality and dynamic interfaces in hero page

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -2,12 +2,54 @@
 
 namespace App\Livewire;
 
+use App\Models\Category;
+use App\Models\Product;
 use Livewire\Component;
+use Livewire\Attributes\Title;
+use Livewire\Attributes\Url;
+use Livewire\WithPagination;
 
+#[Title(content: 'Products Page | Spartan Commerce')]
 class ProductsPage extends Component
 {
+    use WithPagination;
+
+    #[Url]
+    public $selected_categories = [];
+
+    
+    #[Url]
+    public $featured;
+
+    #[Url]
+    public $on_sale;
+
+    #[Url]
+    public $price_range = 800;
+
     public function render()
     {
-        return view('livewire.products-page');
+        $productQuery = Product::query()->where('is_active', 1); 
+
+        if(!empty($this->selected_categories)){
+            $productQuery->whereIn('category_id', $this->selected_categories);
+        }
+
+        if ($this->featured){
+            $productQuery->where('is_featured', 1);
+        }
+        if ($this->on_sale){
+            $productQuery->where('on_sale', 1);
+        }
+
+        if ($this->price_range){
+            $productQuery->whereBetween('price', [0, $this->price_range]);
+        }
+
+
+        return view('livewire.products-page', [
+            'products' => $productQuery->paginate(6),
+            'categories' => Category::where('is_active', 1)->get(['id', 'name', 'slug']),
+        ]);
     }
 }
